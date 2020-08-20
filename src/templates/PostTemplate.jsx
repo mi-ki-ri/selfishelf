7b2730a9bc7b1b2e2c260f82bee96cba02a6a2d1
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2020-08-20 14:03:54 +0900

    marked

--- src/templates/PostTemplate.jsx
+++ src/templates/PostTemplate.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import marked from "marked"
 
 export default ({ data }) => {
   const story = data.allWordpressPost.edges[0].node
+  
+  const mkContent = marked( story.content );
+  
   return (
     <Layout>
       <h2>{story.title}</h2>
       <div>
-        <div dangerouslySetInnerHTML={{ __html: story.content }} />
+        <div dangerouslySetInnerHTML={{ __html: mkContent }} />
         <p>{story.date}</p>
       </div>
     </Layout>
