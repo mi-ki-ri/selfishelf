da16e0fe897479ea0e4fb8efccff49b98f72d3a5
Suzuki Takafumi <suzukitakafumifcc@gmail.com>
2020-08-20 14:16:02 +0900

    marked_v2

--- src/templates/PostTemplate.jsx
+++ src/templates/PostTemplate.jsx
@@ -1,18 +1,36 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
-import marked from "marked"
+
 
 export default ({ data }) => {
+  // Create reference instance
+  const marked = require('marked');
+  
   const story = data.allWordpressPost.edges[0].node
   
   const mkContent = marked( story.content );
+  
+  const mdClass = css`
+    & strong::before{content:"**";}
+    & strong::after{content:"**";}
+    & em::before{content: "*";}
+    & em::after{content: "*";}
+    
+    & code::before{content:"\`";}
+    & code::after{content:"\`;}
+    
+  `
+  
   
   return (
     <Layout>
       <h2>{story.title}</h2>
       <div>
-        <div dangerouslySetInnerHTML={{ __html: mkContent }} />
+        <div
+          dangerouslySetInnerHTML={{ __html: mkContent }}
+          className={mdClass}
+        />
         <p>{story.date}</p>
       </div>
     </Layout>
